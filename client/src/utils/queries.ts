bc0e14f729e2647d6814a0ba786051dd23951702
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-10-30 21:09:13 -0400

    queries.ts created

---
+++ client/src/utils/queries.ts
@@ -0,0 +1,30 @@
+
+import { gql } from '@apollo/client';
+
+
+// queries.ts: This will hold the query GET_ME, 
+// which will execute the me query set up using Apollo Server.
+
+export const GET_ME = gql`
+  query me {
+
+
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+
+
+    }
+    `;
+// queries.ts: This will hold the query GET_ME, 
+
